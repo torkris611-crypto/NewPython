f5ebedc88cb1e8a5e2535bfc052e64f0a1336f3c
Kris <kristinaastapova333@gmail.com>
2026-02-05 12:18:27 +0300

    new commit

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 
 def print_hi(name):
-    print(f'Hi, {name}')
-    a = 1
-    b = 2
-    res = a + b
-    print(res)
-    div = b / a
-    # интересно, что теперь делать?
-    print(f"div = {div}")
-    # что то
-
-
+    print(f'Новая вертка newREP {name}")
 
 
 
